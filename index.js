6ffa16ceb814c811e3629393b361f9f915757813
william <william.binitie@babbangona.com>
2022-07-16 19:03:00 +0100

    added cors

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const paymentRouter = require("./routes/index");
+
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
